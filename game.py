27bbd1fc689b42b62fc94f422a4734b06a499000
toby <tbcrltn@gmail.com>
2025-03-06 16:15:19 -0700

    basic movement

--- game.py
+++ game.py
@@ -1,8 +1,71 @@
 import pygame
-
+from player import Player
 class Game:
     def __init__(self):
-        pass
+            
+        self.screen = pygame.display.set_mode((700, 700))
+        image = pygame.image.load("sprites/grass.png")
+        self.bgimage = pygame.transform.scale(image, (2000, 2000))
+        self.camera = self.bgimage.get_rect(topleft = (0, 0))
+        self.player = Player(self.screen)
+        self.player_dir = "down"
+        self.player_speed = 5
+
 
     def run_game(self):
-        pass
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            self.screen.blit(self.bgimage, self.camera)
+            
+            self.move()
+            
+            self.player.animate(self.player_dir)
+            pygame.time.Clock().tick(60)
+            pygame.display.flip()
+
+        pygame.quit()
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.player_dir = "up"
+            self.player.moving = True
+            self.move_camera("down")
+        elif keys[pygame.K_DOWN]:
+            self.player_dir = "down"
+            self.player.moving = True
+            self.move_camera("up")
+        elif keys[pygame.K_LEFT]:
+            self.player_dir = "left"
+            self.player.moving = True
+            self.move_camera("right")
+        elif keys[pygame.K_RIGHT]:
+            self.player_dir = "right"
+            self.player.moving = True
+            self.move_camera("left")
+        else:
+            self.player.moving = False
+
+    def move_camera(self, dir):
+        if dir == "right":
+            self.camera.x += self.player_speed
+        if dir == "left":
+            self.camera.x -= self.player_speed
+        if dir == "up":
+            self.camera.y -= self.player_speed
+        if dir == "down":
+            self.camera.y += self.player_speed
+        
+        if self.camera.x > 0:
+            self.camera.x = -1000
+        if self.camera.y > 0:
+            self.camera.y = -1000
+        if self.camera.x < -1000:
+            self.camera.x = 0
+        if self.camera.y < -1000:
+            self.camera.y = 0
+        print(self.camera)
--- player.py
+++ player.py
@@ -1,6 +1,65 @@
 import pygame
 class Player:
     
-    def __init__(self):
+    def __init__(self, screen):
         self.x, self.y = 0, 0
-        self.image = pygame.image.load("")
+        image = pygame.image.load("sprites/player3.png")
+        self.image = pygame.transform.scale(image, (50, 50))
+        self.player = self.image.get_rect(topleft = (300, 300))
+        self.screen = screen
+        self.timer = 10
+        self.animation = 3
+        self.anim_speed = 8
+        self.moving = False
+        
+    def animate(self, dir):
+        if self.moving:
+            self.timer += 1
+            if self.timer > self.anim_speed:
+                self.animation += 1
+                self.timer = 0
+                if self.animation > 4:
+                    self.animation = 1
+            if dir == "down":
+                if self.animation == 1:
+                    new_image = pygame.image.load("sprites/player2.png")
+                elif self.animation == 2 or self.animation == 4:
+                    new_image = pygame.image.load("sprites/player3.png")
+                elif self.animation == 3:
+                    new_image = pygame.image.load("sprites/player4.png")
+            if dir == "up":
+                if self.animation == 1:
+                    new_image = pygame.image.load("sprites/player14.png")
+                elif self.animation == 2 or self.animation == 4:
+                    new_image = pygame.image.load("sprites/player15.png")
+                elif self.animation == 3:
+                    new_image = pygame.image.load("sprites/player16.png")
+            if dir == "right":
+                if self.animation == 1:
+                    new_image = pygame.image.load("sprites/player5.png")
+                elif self.animation == 2 or self.animation == 4:
+                    new_image = pygame.image.load("sprites/player6.png")
+                elif self.animation == 3:
+                    new_image = pygame.image.load("sprites/player8.png")
+            if dir == "left":
+                if self.animation == 1:
+                    new_image = pygame.image.load("sprites/player10.png")
+                elif self.animation == 2 or self.animation == 4:
+                    new_image = pygame.image.load("sprites/player9.png")
+                elif self.animation == 3:
+                    new_image = pygame.image.load("sprites/player12.png")
+            
+        else:
+            if dir == "up":
+                new_image = pygame.image.load("sprites/player15.png")
+            elif dir == "down":
+                new_image = pygame.image.load("sprites/player3.png")
+            elif dir == "left":
+                new_image = pygame.image.load("sprites/player9.png")
+            elif dir == "right":
+                new_image = pygame.image.load("sprites/player5.png")
+        
+        
+        scaled_image = pygame.transform.scale(new_image, (50, 50))
+        self.screen.blit(scaled_image, self.player)
+
