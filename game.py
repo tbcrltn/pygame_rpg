cf714a1ec1943f266271f5299b2b5d79ed41b6f6
toby <tbcrltn@gmail.com>
2025-03-10 15:30:51 -0600

    completed the tile set

--- game.py
+++ game.py
@@ -1,27 +1,32 @@
 import pygame
 from player import Player
-from obj import *
-import pytmx
+from map import *
+from pytmx.util_pygame import load_pygame
+
 class Game:
     def __init__(self):
-        self.map = pytmx.load_pygame("map.json")
-        self.screen = pygame.display.set_mode((self.map.width * self.map.tilewidth, self.map.height * self.map.tileheight))
+        self.screen = pygame.display.set_mode((700,700))
         self.player = Player(self.screen)
         self.player_dir = "down"
         self.player_speed = 5
+        self.tile_group = pygame.sprite.Group()
+        self.collision_group = pygame.sprite.Group()
+        self.tmx_data = load_pygame("maps/map1.tmx")
+        self.dx = 0
+        self.dy = 0
+
+        self.load_map()
 
 
     def run_game(self):
+        pygame.init()
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
         
-
-            self.draw_map()
-            
-            
+            self.tile_group.draw(self.screen)
             self.player.animate(self.player_dir)
             self.move()
             pygame.time.Clock().tick(60)
@@ -31,35 +36,49 @@ class Game:
     
     
     
-    def draw_map(self):
-        for layer in self.map.visible_layers:
-            if isinstance(layer, pytmx.TiledTileLayer):
-                for x, y, gid in layer:
-                    tile = self.map.get_tile_image_by_gid(gid)
-                    if tile:
-                        self.screen.blit(tile, (x*self.map.tilewidth, y*self.map.tileheight))
-    
-    
     def move(self):
+        self.dx, self.dy = 0,0
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             self.player_dir = "up"
+            self.dy = self.player_speed
             self.player.moving = True
-            self.move_camera("down")
         elif keys[pygame.K_DOWN]:
             self.player_dir = "down"
+            self.dy = -self.player_speed
             self.player.moving = True
-            self.move_camera("up")
         elif keys[pygame.K_LEFT]:
             self.player_dir = "left"
+            self.dx = self.player_speed
             self.player.moving = True
-            self.move_camera("right")
         elif keys[pygame.K_RIGHT]:
             self.player_dir = "right"
+            self.dx = -self.player_speed
             self.player.moving = True
-            self.move_camera("left")
         else:
             self.player.moving = False
 
-    def move_camera(self, dir):
-        pass
+
+
+        for tile in self.tile_group:
+            tile.rect.x += self.dx
+            tile.rect.y += self.dy
+
+
+    
+
+
+
+    def load_map(self):
+        for layer in self.tmx_data.layers:
+            if layer.name in ("Tile Layer 1", "Extra"):
+                for x, y, surf in layer.tiles():
+                    pos = (x*50, y*50)
+                    Tile(pos= pos, surface = surf, groups = self.tile_group)
+
+            if layer.name in ("Collide"):
+                for x, y, surf in layer.tiles():
+                    pos = (x*50, y*50)
+                    object = Tile(pos = pos, surface = surf, groups = self.tile_group)
+                    self.collision_group.add(object)
+                    
--- map.py
+++ map.py
@@ -1,29 +1,33 @@
-import pygame, csv
+import pygame
+from pytmx.util_pygame import load_pygame
 
 
-TILE_SIZE = 32
-MAP_FILES = ["map1/map_Tile Layer 1.csv", "map1/map_Extra.csv", "map1/map_Collide.csv"]
-TILE_SHEET_FILE = "tiles/forest_tiles.png"
-TILE_COLUMNS = 16
 
 
-tile_sheet = pygame.image.load(TILE_SHEET_FILE)
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, pos, surface, groups):
+        super().__init__(groups)
+        self.image = pygame.transform.scale(surface, (50, 50))
+        self.rect = self.image.get_rect(topleft = pos)
+
+class OBJ(pygame.sprite.Sprite):
+    def __init__(self, pos, surface, groups):
+        super().__init__(groups)
+        self.image = pygame.transform.scale(surface, (50, 50))
+        self.rect = self.image.get_rect(topleft = pos)
+
+    
+    def collision(self, obj):
+        if self.rect.colliderect(obj):
+            print("collision")
+            return True
+        return False
 
 
-def load_tiles(sheet, tile_size, columns):
-    tiles = []
-    sheet_width, sheet_height = sheet.get_size()
-    rows = sheet_height // tile_size
-
-    for y in range(rows):
-        for x in range(columns):
-            rect = pygame.Rect(x*tile_size, y*tile_size, tile_size, tile_size)
-            tiles.append(sheet.subsurface(rect))
-
-    return tiles
 
 
-tiles = load_tiles(tile_sheet, TILE_SIZE, TILE_COLUMNS)
 
-def load_csv_map():
-    pass
+
+
+
